20e4d6c53c9bd8a0810d3eed901c0281165a45ae
Iislsdum <Iislsdum@gmail.com>
2017-05-09 14:23:21 -0600

    Add handler for the calculate button

--- Daily Programmer 2017-05-08/Form1.vb
+++ Daily Programmer 2017-05-08/Form1.vb
@@ -1,3 +1,31 @@
 ﻿Public Class Form1
 
+	Sub Calculate() Handles SubmitButton.Click
+		Dim numbers As String() = Input.Text.Split({" "c})
+
+		Dim compare As Comparison(Of String) =
+			Function(x As String, y As String)
+				For i As Integer = 0 To Math.Min(x.Length, y.Length)
+					Dim xInt As Integer = Val(x.Chars(i))
+					Dim yInt As Integer = Val(y.Chars(i))
+
+					If xInt <> yInt Then
+						Return xInt - yInt
+					End If
+				Next
+
+				If x.Length = y.Length Then
+					Return 0
+				End If
+
+				Dim result As Integer
+				If x.Length > y.Length Then
+					result = compare(x.Substring(y.Length), y)
+				Else
+					result = compare(x, y.Substring(x.Length))
+				End If
+				Return result
+			End Function
+	End Sub
+
 End Class
